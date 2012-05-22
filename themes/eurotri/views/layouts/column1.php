0b8ab355be1018801ce0f493849afc6fa36e7c3c
Dani Gomez <dani.vlc89@gmail.com>
2012-05-22 11:18:05 +0200

    Borrado tema euro...

--- themes/eurotri/views/layouts/column1.php
+++
@@ -1,8 +0,0 @@
-<?php $this->beginContent('webroot.themes.'.Yii::app()->theme->name.'.views.layouts.main'); ?>
-
-<div id="maincontent">
-    <?php $this->widget('zii.widgets.CBreadcrumbs', array('links'=>$this->breadcrumbs,)); ?><!-- breadcrumbs -->
-    <?php echo $content; ?>
-</div>
-
-<?php $this->endContent(); ?>
